d63c6659109af48f79bb5eb47c679e682c288fb5
Dybalux <luchicapo@gmail.com>
2022-01-24 00:12:50 -0300

    Add MongoDb class

--- index.js
+++ index.js
@@ -1,90 +1,129 @@
-const { response } = require('express')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+require("dotenv").config();
+require("./mongo");
 
-app.use(cors())
-app.use(express.json())
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Note = require("./models/Note");
+const { request, response } = require("express");
+const notFound = require("./middleware/notFound");
+const handleErrors = require("./middleware/handleErrors");
 
-let notes = [
-	{
-		id: 1,
-		content: 'Te con menta',
-		date: '2022-01-20T12:45:12.1234',
-		important: true,
-	},
-	{
-		id: 2,
-		content: 'Cafe',
-		date: '2022-01-21T12:05:12.1234',
-		important: true,
-	},
-	{
-		id: 3,
-		content: 'Mate',
-		date: '2022-01-22T11:45:12.1234',
-		important: true,
-	},
-]
+app.use(cors());
+app.use(express.json());
+app.use('/images', express.static('images'))
 
-app.get('/', (request, response) => {
-	response.send('<h1>Hola mundo!</h1>')
-})
+Sentry.init({
+  dsn: "https://94ae107199fb478fb3458d16db38b36f@o1124962.ingest.sentry.io/6163409",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
 
-app.get('/api/notes', (request, response) => {
-	response.json(notes)
-})
-app.get('/api/notes/:id', (request, response) => {
-	//recibe todas las notas
-	const id = Number(request.params.id)
-	//recibe la id de nota que necesitamos
-	const note = notes.find((note) => note.id === id)
-	///muestra la nota
-	if (note) {
-		response.json(note)
-	} else {
-		response.status(404).end()
-	}
-})
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	notes = notes.filter((note) => note.id !== id)
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
-	response.status(204).end()
-})
+let notes = [];
 
-app.post('/api/notes', (request, response) => {
-	const note = request.body
+app.get("/", (request, response) => {
+  console.log(request.ip)
+  console.log(request.ips)
+  console.log(request.originalUrl)
+  response.send("<h1><br>Hola mundo!</br></h1>");
+});
 
-	if (!note || !note.content) {
-		return response.status(400).json({
-			error: 'note.content is missing',
-		})
-	}
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+app.get("/api/notes/:id", (request, response, next) => {
+  //recibe todas las notas
+  const { id } = request.params;
 
-	const ids = notes.map((note) => note.id)
-	const maxId = Math.max(...ids)
-	const newNote = {
-		id: maxId + 1,
-		content: note.content,
-		important: typeof note.important !== 'undefined' ? note.important : false,
-		date: new Date(),
-	}
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-	notes = [...notes, newNote]
+app.put("/api/notes/:id", (request, response, next) => {
+  const { id } = request.params;
+  const note = request.body;
 
-	response.status(201).json(newNote)
-})
+  const newNoteInfo = {
+    content: note.content,
+    important: note.important,
+  };
 
-app.use((request,response) => {
-	response.status(404).json({
-		error: 'Not found',
-	})
-})
+  Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
+    .then((result) => {
+      response.json(result);
+    })
+    .catch(next);
+});
 
-const PORT = process.env.PORT || 3001
+app.delete("/api/notes/:id", (request, response, next) => {
+  const { id } = request.params;
 
+  Note.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch(next());
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response, next) => {
+  const note = request.body;
+
+  if (!note.content) {
+    return response.status(400).json({
+      error: 'requiered "content" field is missing',
+    });
+  }
+
+  const newNote = Note({
+    content: note.content,
+    date: new Date(),
+    import: note.important || false,
+  });
+
+  newNote
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((err) => next(err));
+});
+
+app.use(notFound);
+
+app.use(Sentry.Handlers.errorHandler());
+app.use(handleErrors);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-	console.log(`Server running on por ${PORT}`)
-})
+  console.log(`Server running on por ${PORT}`);
+});
